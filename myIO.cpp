38c56d6e32a0fe3c48cb3c3a120133be853d9e34
proxy-hatch <wyx_100@hotmail.com>
2017-10-17 23:01:51 -0700

    Oct 17. End of the night.

--- myIO.cpp
+++ myIO.cpp
@@ -11,8 +11,7 @@
 //% Below, edit to list any people who helped you with the code in this file,
 //%      or put 'None' if nobody helped (the two of) you.
 //
-// Helpers: _everybody helped us/me with the assignment (list names or put 'None')__
-// All TAs and the collective effort of the entire class through course piazza forum (including Dr Scratchley)
+// Helpers: All TAs and the collective effort of the entire class through course piazza forum (including Dr Scratchley)
 // Also, list any resources beyond the course textbooks and the course pages on Piazza
 // that you used in making your submission.
 //
@@ -38,6 +37,9 @@
  * These functions may be re-implemented later in the course.
  */
 
+/* Note by Shawn:
+ * The implementation currently design does NOT support any sort of (intentional) deadlock handling
+ */
 #include <unistd.h>			// for read/write/close
 #include <fcntl.h>			// for open/creat
 #include <sys/socket.h> 		// for socketpair
@@ -46,7 +48,6 @@
 #include <mutex>
 #include <condition_variable>
 #include <vector>
-#include <exception>
 
 #define DEFAULT_CONTAINER_SIZE 50
 #define _DEBUG
@@ -55,7 +56,9 @@
 #include <iostream>		// cerr
 #endif
 
-struct socketInfo {
+
+
+struct sockInfo {
 	const int fd;	// negative file descriptor is invalid
 	int buffCnter;// used to keep track of how many chars are inside the buffer at a given moment
 				  // Positive: buffer filled with char awaiting to be read
@@ -63,28 +66,30 @@ struct socketInfo {
 				  // Zero: buffer empty (ready to accept new write requests from either direction)
 	mutable std::mutex mut;		// used to update buffCnter variable
 	mutable std::condition_variable cond;// used to wait and sleep threads calling myTcdrain()
+	mutable std::condition_variable cond;// used to wait and sleep threads calling myTcdrain()
+
 
 	// constructor
-	socketInfo(const int fildes) : fd(fildes), buffCnter(0){}
+	sockInfo(const int fildes) : fd(fildes), buffCnter(0){}
 }/*socketPairInfo*/;
 // typedef not needed anymore in C++: https://stackoverflow.com/a/252867/5340330
 
 // instantiate vector container
-std::vector<socketInfo*> sockList (DEFAULT_CONTAINER_SIZE);
+std::vector<sockInfo*> sockList (DEFAULT_CONTAINER_SIZE);
 
 // |-------------------------------------------------------------------------|
 // |                          Helper Functions                               |
 // |-------------------------------------------------------------------------|
 
 // Retrieve the socketPairInfo dynamic struct related to files
-socketInfo* getSocketInfo(const int fd) {
+sockInfo* getSockInfo(const int fd) {
 	if (fd < 0) {
 #ifdef _DEBUG
-		std::cerr << "getSocketInfo(" << fd << ") invalid: Negative." << std::endl;
+		std::cerr << "getSockInfo(" << fd << ") invalid: Negative." << std::endl;
 #endif
-		throw std::out_of_range("Negative file descriptor");
+		return nullptr;
 	}
-	socketInfo* sock;
+	sockInfo* sock;
 	// search for socketPair
 	try {
 		sock = sockList.at(fd); // unordered_map::at throws an out-of-range
@@ -92,10 +97,25 @@ socketInfo* getSocketInfo(const int fd) {
 #ifdef _DEBUG
 		std::cerr << "Out of Range error: " << oor.what() << '\n';
 #endif
-		throw std::out_of_range("file descriptor bigger than vector size");
+		return nullptr;
 	}
 
 	return sock;
+}
+
+// insert sockInfo entry at vector.at(fd)
+// return 0 upon success.
+// return 1 if there is an error (index occupied for example)
+int addSockInfo(sockInfo* newSock, int fd){
+	// error handle
+	if(sockList.at(fd)){
+#ifdef _DEBUG
+		std::cerr << "addSockInfo(" << fd << ") failed: sockInfo already created for fd." << std::endl;
+#endif
+		return 1;
+	}
+	sockList.at(fd) =newSock;
+	return 0;
 }
 
 // |-------------------------------------------------------------------------|
@@ -108,14 +128,25 @@ int mySocketpair(int domain, int type, int protocol, int des[2]) {
 	// success
 	if (returnVal != -1) {
 		// des should now be filled with correct file descriptor, so we extract them for our struct
-		socketInfo* newSock0 = new socketInfo(des[0]);
-		socketInfo* newSock1 = new socketInfo(des[1]);
+		sockInfo* newSock0 = new sockInfo(des[0]);
+		sockInfo* newSock1 = new sockInfo(des[1]);
 
 		// add vector entry for each socket
-
-
+		// if the first one returned non-zero, do not proceed with the second one
+		// if the second one returned with non-zero, delete the first one
+		// Theoretically neither socket should be occupied already
+		if(addSockInfo(newSock0, des[0])){
+			// error handle
+			returnVal = -1;
+		}
+		else if(addSockInfo(newSock1, des[1])){
+			// delete first one if it did not give error
+			if(returnVal != -1)
+				sockList.at(des[0]) = nullptr;
+			// error handle
+			returnVal = -1;
+		}
 	}
-
 	return returnVal;
 }
 
@@ -128,8 +159,9 @@ int myCreat(const char *pathname, mode_t mode) {
 }
 
 ssize_t myRead(int fildes, void* buf, size_t nbyte) {
-
-	socketInfo* sock = getSocketInfo(fildes);
+	if(buffCnter)
+	lock_guard
+	sockInfo* sock = getSockInfo(fildes);
 
 	return myReadcond(fildes, buf, nbyte, 1, 0, 0);
 }
@@ -143,19 +175,22 @@ ssize_t myWrite(int fildes, const void* buf, size_t nbyte) {
 // After talking with different TAs, it seems that one myClose(3) should close down its paired socket as well
 // i.e., calling myClose(4) as well
 int myClose(int fd) {
+
 	return close(fd);
 }
 
-// tcdrain() blocks the calling thread until all previously written characters have actually been sent
+// myTcdrain() blocks the calling thread until all previously written characters have actually been sent
 int myTcdrain(int des) { //is also included for purposes of the course.
 	return 0;
 }
 
 // as suggested in instruction document and by the TAs, myReadCond() is called inside of myRead()
-// It is capable to set a min: 'min' and max: 'n'
+// It is capable to set a min: 'min' and max: 'n'. min < n of course.
 // It is also capable of setting timeout, but that is beyond the scope of this assignment
 // Scenarios: buffer has x chars
 // x > n: read n and return
+// x < n && x > min: return x with x bytes read
+// x < min: block until x = min. Return x with x bytes read
 int myReadcond(int des, void * buf, int n, int min, int time, int timeout) {
 	return wcsReadcond(des, buf, n, min, time, timeout);
 }
