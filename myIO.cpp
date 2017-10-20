628439fa7affc29470316f43c4d886088c076e8d
proxy-hatch <wyx_100@hotmail.com>
2017-10-19 18:39:54 -0700

    Oct 19 Night. Corrected many design logics. Completed but Bugged.

--- myIO.cpp
+++ myIO.cpp
@@ -57,7 +57,8 @@
 #endif
 
 struct sockInfo {
-	const int fd;	// negative file descriptor is invalid
+	const int fd;
+	int pairedFd;	// set to -1 if the paired socket gets close()d.
 	int buffCnter;// used to keep track of how many chars are inside the buffer at a given moment
 				  // Positive: buffer at file descriptor filled with char awaiting to be read
 				  // Negative: Invalid. exception
@@ -66,8 +67,8 @@ struct sockInfo {
 	mutable std::condition_variable cv;	// used to wait and notify threads calling myTcdrain()
 
 	// constructor
-	sockInfo(const int fildes) :
-			fd(fildes), buffCnter(0) {
+	sockInfo(const int fildes, const int pairedFd) :
+			fd(fildes), pairedFd(pairedFd), buffCnter(0) {
 	}
 }/*socketPairInfo*/;
 // typedef not needed anymore in C++: https://stackoverflow.com/a/252867/5340330
@@ -107,15 +108,22 @@ sockInfo* getSockInfo(const int fd) {
 // insert sockInfo entry at vector.at(fd)
 // return 0 upon success.
 // return 1 if there is an error
-int addSockInfo(const sockInfo* newSock, const int fd) {
+int addSockInfo(const sockInfo* newSock) {
+	if(!newSock){
+#ifdef _DEBUG
+		std::cerr << "sockInfo obj ptr passed invalid" << std::endl;
+#endif
+		return 1;
+	}
+
 	sockInfo* sock;
 
 	// error handle: fd out of range
 	try {
-		sock = sockList.at(fd); // vector::at() throws an out-of-range
+		sock = sockList.at(newSock->fd); // vector::at() throws an out-of-range
 	} catch (const std::out_of_range& oor) {
 #ifdef _DEBUG
-		std::cerr << "Out of Range error at addSockInfo(" << fd << "): "
+		std::cerr << "Out of Range error at addSockInfo(" << newSock->fd << "): "
 				<< oor.what() << std::endl;
 #endif
 		return 1;
@@ -124,7 +132,7 @@ int addSockInfo(const sockInfo* newSock, const int fd) {
 	// error handle: sockInfo already exist
 	if (sock) {
 #ifdef _DEBUG
-		std::cerr << "addSockInfo(" << fd
+		std::cerr << "addSockInfo(" << newSock->fd
 				<< ") failed: sockInfo already created for fd" << std::endl;
 #endif
 		return 1;
@@ -132,7 +140,7 @@ int addSockInfo(const sockInfo* newSock, const int fd) {
 
 	// Lock vector upon modifying
 	std::lock_guard<std::mutex> lk(vectorMut);
-	sockList.at(fd) = const_cast<sockInfo*>(newSock); // compiler error of violating const of newSock: Force write with const_cast
+	sockList.at(newSock->fd) = const_cast<sockInfo*>(newSock); // compiler error of violating const of newSock: Force write with const_cast
 													  // https://stackoverflow.com/a/731130
 	return 0;
 }
@@ -175,17 +183,17 @@ int mySocketpair(int domain, int type, int protocol, int des[2]) {
 	// success
 	if (returnVal != -1) {
 		// des should now be filled with correct file descriptor, so we extract them for our struct
-		sockInfo* newSock0 = new sockInfo(des[0]);
-		sockInfo* newSock1 = new sockInfo(des[1]);
+		sockInfo* newSock0 = new sockInfo(des[0],des[1]);
+		sockInfo* newSock1 = new sockInfo(des[1],des[0]);
 
 		// add vector entry for each socket
 		// if the first one returned non-zero, do not proceed with the second one
 		// if the second one returned with non-zero, delete the first one
 		// Theoretically neither socket should be occupied already
-		if (addSockInfo(newSock0, des[0])) {
+		if (addSockInfo(newSock0)) {
 			// error handle
 			returnVal = -1;
-		} else if (addSockInfo(newSock1, des[1])) {
+		} else if (addSockInfo(newSock1)) {
 			// delete first one if it did not give error
 			if (returnVal != -1)
 				sockList.at(des[0]) = nullptr;
@@ -213,68 +221,119 @@ ssize_t myRead(int fildes, void* buf, size_t nbyte) {
 	// If myReadcond() is blocked waiting to read,and then the socket closes. It will return with -1 (no hang)
 	int retVal = myReadcond(fildes, buf, nbyte, 1, 0, 0);
 
-	if (retVal == -1 || !readSock) {	// error or non-socket
+	if (retVal == -1 || !readSock) {	// error or non-socket: do not continue
 #ifdef _DEBUG
 		if (retVal == -1)
 			std::cerr << "myRead(" << fildes << ", buf, " << nbyte << ") failed"
 					<< std::endl;
-#endif _DEBUG
-		return retVal;
-	}
+#endif
+	} else {
+		// at least 1 byte was read. lock and update buffCnter
+		// We are making the assumption here that in between myReadcon() returning and the following line, there has been no new write to fd.
+		/* this is a valid assumption because:
+		 * if myWrite(fd) happened before myReadcond(fd) was called, myReadcond(fd) would immediately return, becoming the next to hold mutex
+		 * If myWrite(fd) was called after myReadcond(fd) was called, myReadcond(fd) would immediately return after myWrite(fd) finishes, becoming the next to hold mutex
+		 * having ANOTHER writer acquiring mutex in BETWEEN - the myWrite(fd) mentioned above releasing mutex and myReadcond(fd) acquiring mutex - is highly unlikely.
+		 * having ANOTHER tcDrain() thread acquiring mutex in between - does not pose a contradiction.
+		 * having ANOTHER tcDrain() thread acquiring mutex AFTER myReacond() returns - does not pose a contradiction either
+		 *
+		 */
+		{
+			std::lock_guard<std::mutex> lk(readSock->buffMut);
+			readSock->buffCnter = readSock->buffCnter - retVal;
 
-	// at least 1 byte was read. lock and update buffCnter
-	// We are making the assumption here that in between myReadcon() returning and the following line, there has been no new write to fd.
-	/* this is a valid assumption because:
-	 * if myWrite(fd) happened before myReadcond(fd) was called, myReadcond(fd) would immediately return, becoming the next to hold mutex
-	 * If myWrite(fd) was called after myReadcond(fd) was called, myReadcond(fd) would immediately return  after myWrite(fd) finishes, becoming the next to hold mutex
-	 * having ANOTHER writer acquiring mutex in between - the myWrite(fd) mentioned above releasing mutex and myReadcond(fd) acquiring mutex - is highly unlikely.
-	 * having ANOTHER tcDrain() thread acquiring mutex in between - does not pose a contradiction.
-	 */
-	{
-		std::lock_guard<std::mutex> lk(readSock->buffMut);
-		readSock->buffCnter = readSock->buffCnter - retVal;
-	}
 
-	// While holding the same mutex for notify()ing seems necessary for precise scheduling,
-	// (i.e., tcDrain()::wait()s should happen before or after the notify(), not at the same time.)
-	// it is inefficient to do so.	see for more: https://stackoverflow.com/a/17102100
-	// Therefore we call notify_all only after immediate release of mutex
-	/* "
-	 * The notifying thread does not need to hold the lock on the same mutex as the one held by the waiting thread(s);
-	 * in fact doing so is a pessimization, since the notified thread would immediately block again, waiting for the notifying thread to release the lock
-	 * ...... Notifying while under the lock may nevertheless be necessary when precise scheduling of events is required
-	 * " - http://en.cppreference.com/w/cpp/thread/condition_variable/notify_one
-	 */
-	// notify_all instead of notify_one is called here to handle the case of potentially multiple tcDrain waiting on fd.
-	readSock->cv.notify_all();
+			// Holding buffMut for notify()ing is NOT inefficient because we are not holding pairedSock->buffMut
+			// (see how it CAN be inefficient: http://en.cppreference.com/w/cpp/thread/condition_variable/notify_all
+			// and https://stackoverflow.com/a/17102100).
+			// It is essential for precise scheduling:
+			/* Consider the following scenario (if notify_all() occurs after releasing buffMut):
+			 * threadA::mySend(pairedFd) && set buffCnter to n bytes -> threadA::tcDrain()::wait(pairedFd) -> threadB::myRead(fd) resets buffCnter=0 and release buffMut
+			 *  -> threadC::mySend(pairedFd) && set buffCnter to n bytes again -> threadC::tcDrain::wait(pairedFd) -> threadB::myRead()::notify_all().
+			 * Now, both threadA AND threadC are woken up, even though buffCnter != 0 and threadC shouldn't be.
+			*/
+			if (!(readSock->buffCnter)) {
+				sockInfo* pairedSock = getSockInfo(readSock->pairedFd);
+				if(pairedSock){		// pairedSock==nullptr --> socket already closed. Nothing to do.
+					// notify_all instead of notify_one is called here to handle the case of potentially multiple tcDrain waiting on fd.
+					// note that we do not need to hold pairedSock->buffMut
+					pairedSock->cv.notify_all();
+				}
+			}
+		}
+	}
+	return retVal;
+}
+
+ssize_t myWrite(int fildes, const void* buf, size_t nbyte) {\
+	ssize_t retVal;
+	sockInfo* writeSock = getSockInfo(fildes);
+	sockInfo* pairedSock;
+
+	// fd is a socket: Hold bufMut before write(), and update pairedSock->buffCnter accordingly afterwards
+	if (writeSock && (pairedSock = getSockInfo(writeSock->pairedFd))) {
+		std::lock_guard<std::mutex> lk(pairedSock->buffMut);
+		retVal = write(fildes, buf, nbyte);
+		// update buffCnter as necessary
+		if (retVal > 0) {
+			pairedSock->buffCnter += retVal;
+		}
+	} else
+		retVal = write(fildes, buf, nbyte);
 
 	return retVal;
 }
 
-ssize_t myWrite(int fildes, const void* buf, size_t nbyte) {
-// check if there is someone holding mutex
-
-	return write(fildes, buf, nbyte);
-}
-
-// After talking with different TAs, it seems that one myClose(3) should close down its paired socket as well
-// i.e., calling myClose(4) as well
+// closes the socket associated with fd but DO NOT close its paired socket.
+// However, threads that are waiting on tcDrain(pairedFd)::pairedSock->cv.wait() should be waken up.
 int myClose(int fd) {
+	int retVal;
+	sockInfo* closeSock = getSockInfo(fd);
+	sockInfo* pairedSock;
 
-	return close(fd);
+	if (closeSock) {
+		{
+			// close() should not happen concurrently with read()/write()/wait(), therefore hold mutex
+			std::lock_guard<std::mutex> lk(closeSock->buffMut);
+			retVal = close(fd);
+			closeSock->buffCnter = 0;	// reset buffCnter to prevent subsequent tcDrain(fd)::wait()
+		}		// fd is now closed. Subsequent write()/read() should fail.
+		closeSock->cv.notify_all();	// free the remaining tcDrain(fd)s
+
+		// make paired socket aware that this socket is closed
+		// It should NOT however, wake the threads blocked by tcDrain(pairedFd). That is the responsibility of myRead(pairedFd)
+		if ((pairedSock = getSockInfo(closeSock->pairedFd))) {
+			pairedSock->pairedFd = -1;
+		}
+
+		// delete sockInfo* from vector to make room for the potentially re-purposed fd
+		std::unique_lock<std::mutex> lk(vectorMut);
+		sockList.at(closeSock->fd) = nullptr;
+		lk.unlock();
+
+		// Socket should now be safe to delete
+		delete closeSock;
+	} else
+		retVal = close(fd);
+
+	return retVal;
 }
 
 // myTcdrain() blocks the calling thread until all previously written characters have actually been sent
 int myTcdrain(int des) { //is also included for purposes of the course.
-	sockInfo* sock = getSockInfo(des);
-
-	// while reading buffCnter does not cause concurrency issues, it is necessary to hold the mutex before reading,
-	// in case of buffCnter being updated after reading,  causing invalid judgement of whether to wait or not.
-	// e.g., (buffCnter!=0) == true --> myRead() made buffCnter=0 and notify_all() --> myTcdrain() misses notify_all() and now waits despite buffCnter==0
-	std::unique_lock<std::mutex> lk(sock->buffMut);
-	if (sock->buffCnter)
-		sock->cv.wait(lk, [] {return !(sock->buffCnter);});
-
+	// check if des is indeed a socket
+	if (sockInfo* sock = getSockInfo(des)) {
+		// while reading buffCnter does not cause concurrency issues, it is necessary to hold the mutex before reading,
+		// in case of buffCnter being updated after reading,  causing invalid judgement of whether to wait or not.
+		// e.g., (buffCnter!=0) == true --> myRead() made buffCnter=0 and notify_all() --> myTcdrain() misses notify_all() and now waits despite buffCnter==0
+		std::unique_lock<std::mutex> lk(sock->buffMut);
+		if (sock->buffCnter) {
+//			sock->cv.wait(lk, [] {return !(sock->buffCnter);});
+			// while having the buffCnter check here thru the lambda seems like a good idea, we need myClose::notify_all() to be able to wake up tcDrain
+			// Thus the responsibility of buffCnter checking relies on myRead::notify_all()
+			sock->cv.wait(lk);
+		}
+	}
 	return 0;
 }
 
